ec5e55580cf6d511349e622ecd1b4ee958f23aa6
wo9374 <wo9374@naver.com>
2022-02-18 15:18:37 +0900

    Base Activity, Fragment 추가

---
+++ app/src/main/java/com/example/kovid/base/BaseActivity.kt
@@ -0,0 +1,21 @@
+package com.example.kovid.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding>(private val layoutId: Int) : AppCompatActivity() {
+    private var _binding: T? = null
+    val binding: T get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = DataBindingUtil.setContentView(this, layoutId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.unbind()
+    }
+}
---
+++ app/src/main/java/com/example/kovid/base/BaseFragment.kt
@@ -0,0 +1,41 @@
+package com.example.kovid.base
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+abstract class BaseFragment<T : ViewDataBinding>(private val layoutId: Int) : Fragment() {
+    private var _binding: T? = null
+    val binding:T get() = _binding!!
+
+    lateinit var mContext: Context
+    lateinit var navController: NavController
+
+    //return layout resource id
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mContext = context
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.unbind()
+    }
+}
