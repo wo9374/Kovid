dee6fa3cc120c21aafca0fc19d39b89059e214db
wo9374 <55440637+wo9374@users.noreply.github.com>
2022-03-10 23:07:20 +0900

    barChart 마커view 색상 커스텀

--- app/src/main/java/com/project/kovid/extenstion/customview/CovidBarChartMarker.kt
+++ app/src/main/java/com/project/kovid/extenstion/customview/CovidBarChartMarker.kt
@@ -12,6 +12,7 @@ import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.highlight.Highlight
 import com.project.kovid.R
 import com.project.kovid.databinding.CustomMpchartMarkerBinding
+import com.project.kovid.util.StringUtil
 
 @SuppressLint("ViewConstructor")
 class CustomChartMarker(context: Context, layoutResource: Int) : MarkerView(context, layoutResource) {
@@ -29,16 +30,7 @@ class CustomChartMarker(context: Context, layoutResource: Int) : MarkerView(cont
 
     // entry를 content의 텍스트에 지정
     override fun refreshContent(e: Entry, highlight: Highlight?) {
-        tvContent.text = e.y.toInt().toString()
-        tvContent.setTextColor(lightDarkThemeCheck())
+        tvContent.text = StringUtil.getDecimalFormatNum(e.y.toInt())
         super.refreshContent(e, highlight)
-    }
-
-    private fun lightDarkThemeCheck(): Int {
-        return if (context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES) {
-            ContextCompat.getColor(context, R.color.white)
-        } else {
-            ContextCompat.getColor(context, R.color.black)
-        }
     }
 }
