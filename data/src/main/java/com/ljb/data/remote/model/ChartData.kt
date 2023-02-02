dc6214fae8f226877a1bdfe0eed4eb640dc5a03f
jblee9374 <55440637+wo9374@users.noreply.github.com>
2023-02-02 15:52:47 +0900

    refactor: 일일확진자 data class - Data 레이어, Domain 레이어 구분

---
+++ data/src/main/java/com/ljb/data/remote/model/ChartData.kt
@@ -0,0 +1,66 @@
+package com.ljb.data.remote.model
+
+import com.tickaroo.tikxml.annotation.Element
+import com.tickaroo.tikxml.annotation.PropertyElement
+import com.tickaroo.tikxml.annotation.Xml
+
+/**
+ * CovidAPI에서 반환형으로 사용되는 response data class
+ * */
+@Xml(name = "response")
+data class ChartData(
+    @Element(name = "header")
+    val chartHeader: ChartHeader,
+    @Element(name = "body")
+    val chartBody: ChartBody
+)
+
+@Xml(name = "header")
+data class ChartHeader(
+    @PropertyElement(name = "resultCode")
+    val resultCode: Int,
+    @PropertyElement(name = "resultMsg")
+    val resultMsg: String,
+)
+
+@Xml(name = "body")
+data class ChartBody(
+    @Element(name = "items")
+    val chartItems: ChartList,
+    @PropertyElement(name = "numOfRows")
+    val numOfRows: Int,
+    @PropertyElement(name = "pageNo")
+    val pageNo: Int,
+    @PropertyElement(name = "totalCount")
+    val totalCount: Int,
+)
+
+@Xml(name = "items")
+data class ChartList(
+    @Element(name = "item")
+    val chartItem: List<ChartResponse>
+)
+
+@Xml(name = "item")
+data class ChartResponse(
+    @PropertyElement(name = "createDt")   //등록일시분초
+    var createDt: String,
+
+    @PropertyElement(name = "deathCnt")   //사망자 수
+    var deathCnt: Int,
+
+    @PropertyElement(name = "decideCnt")  //확진자 수
+    var decideCnt: Int,
+
+    @PropertyElement(name = "seq")        //감염현황 고유값
+    var seq: String,
+
+    @PropertyElement(name = "stateDt")    //기준일
+    var stateDt: String,
+
+    @PropertyElement(name = "stateTime")  //기준시간
+    var stateTime: String,
+
+    @PropertyElement(name = "updateDt")   //수정일시분초
+    var updateDt: String? = "",
+)
---
+++ domain/src/main/java/com/ljb/domain/entity/ChartItem.kt
@@ -0,0 +1,16 @@
+package com.ljb.domain.entity
+
+data class ChartItem(
+    var createDt: String,           //등록일시분초
+    var deathCnt: Int,              //사망자 수
+    var decideCnt: Int,             //확진자 수
+    var seq: String,                //감염현황 고유값
+    var stateDt: String,            //기준일
+    var stateTime: String,          //기준시간
+    var updateDt: String? = "",     //수정일시분초
+)
+
+data class WeekCovid(
+    val day : String,
+    val decideCnt : Int,
+)
