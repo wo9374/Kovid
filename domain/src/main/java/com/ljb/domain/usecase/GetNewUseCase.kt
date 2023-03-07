48b7366830a0e2e09f52af43acf0a1dad575c9ed
jblee9374 <55440637+wo9374@users.noreply.github.com>
2023-03-07 09:44:19 +0900

    fix: Domain Module Hilt 제거

--- domain/src/main/java/com/ljb/domain/usecase/GetNewUseCase.kt
+++ domain/src/main/java/com/ljb/domain/usecase/GetNewUseCase.kt
@@ -4,8 +4,7 @@ import com.ljb.domain.NetworkState
 import com.ljb.domain.entity.News
 import com.ljb.domain.repository.NewRepository
 import kotlinx.coroutines.flow.Flow
-import javax.inject.Inject
 
-class GetNewUseCase @Inject constructor(private val repository: NewRepository){
+class GetNewUseCase constructor(private val repository: NewRepository){
     operator fun invoke(query: String) : Flow<NetworkState<List<News>>> = repository.getNewsData(query)
 }
